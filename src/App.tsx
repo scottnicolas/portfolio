f6d765693da229495d285d1b6b29e7ebd2bcc54b
Scott Nicolas <scottnicolas@sub77-132.uhh.hawaii.edu>
2024-12-16 09:41:06 -1000

    add to about me section and skills section

--- src/App.tsx
+++ src/App.tsx
@@ -62,8 +62,54 @@ const App = () => (
         <div className="max-w-screen-2xl mx-auto sm:px-16">
           <div className="md:block hidden">
             <h1 className="font-dm-sans-serif text-9xl font-bold">
-              SCOTT NICOLAS
+              Scott NICOLAS
             </h1>
+          </div>
+          <div className="p-3 flex">
+            <ul className="flex items-center">
+              <li className="mr-5 shrink-0 text-xs">
+                <a
+                  className="block hover:text-slate-200"
+                  href="https://www.linkedin.com/in/scottnicolas/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  aria-label="LinkedIn (opens in a new tab)"
+                  title="LinkedIn"
+                >
+                  <span className="sr-only">LinkedIn</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                  </svg>
+                </a>
+              </li>
+              <li className="mr-5 shrink-0 text-xs">
+                <a
+                  className="block hover:text-slate-200"
+                  href="https://github.com/scottnicolas"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  aria-label="GitHub (opens in a new tab)"
+                  title="GitHub"
+                >
+                  <span className="sr-only">GitHub</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                  </svg>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -194,9 +240,16 @@ const App = () => (
       </div>
       <div className="max-w-screen-xl mx-auto">
         <p className="flex items-center font-dm-sans-serif text-base md:text-xl text-left pt-4 pr-4 pb-4 pl-0">
-          Hey, I'm Scott Nicolas and I am a web developer who likes to I am
-          currently a student at the University of Hawaii at Hilo. I look
-          forward to learning about new technologies and innovative designs.
+          I’m a passionate frontend developer and designer with a strong
+          foundation in building modern, user-friendly web applications. Skilled
+          in React, TypeScript, and TailwindCSS, I have a keen eye for design
+          and a commitment to creating seamless digital experiences.
+        </p>
+        <p className="flex items-center font-dm-sans-serif text-base md:text-xl text-left pt-4 pr-4 pb-4 pl-0">
+          With a background in Computer Science and hands-on experience in
+          developing full-stack applications, I enjoy turning ideas into
+          functional, visually appealing products. I’m always eager to learn,
+          innovate, and collaborate on projects that make a difference.
         </p>
       </div>
     </section>
@@ -210,7 +263,7 @@ const App = () => (
         </div>
 
         <div className="grid grid-cols-3">
-          <div>
+          <div className="border border-black rounded-xl shadow-lg p-5 mx-5">
             <h2 className="font-dm-serif text-xl">Languages</h2>
             <ul className="flex flex-wrap">
               <li className="mr-1.5 mt-2">
@@ -251,7 +304,7 @@ const App = () => (
             </ul>
           </div>
 
-          <div>
+          <div className="border border-black rounded-xl shadow-md p-5 mx-5">
             <h2 className="font-dm-serif text-xl">Frameworks & Libraries</h2>
             <ul className="flex flex-wrap">
               <li className="mr-1.5 mt-2">
@@ -272,7 +325,7 @@ const App = () => (
             </ul>
           </div>
 
-          <div>
+          <div className="border border-black rounded-xl shadow-md p-5 mx-5">
             <h2 className="font-dm-serif text-xl">Developer Tools</h2>
             <ul className="flex flex-wrap">
               <li className="mr-1.5 mt-2">
